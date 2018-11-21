5be1ee7d789eb7874d01ff3f3757c89b34ce2ffa
Jo <jonathan.jacquet62@laposte.net>
2018-11-21 11:18:32 +0100

    admin page

--- admin.php
+++ admin.php
@@ -1,38 +1,55 @@
-<!doctype html>
-<html class="no-js" lang="">
+<?php
+include "template/header.php";
+?>
+<!--
+~~~~~~~~~| catégories |~~~~~~~~
+-->
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<div class="card row">
+<div class="card col-sm-12 col-md-5">
+  <div class="card-body">
+    <h5 class="card-title">Catégorie</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Créer/Supprimer des catégories</h6>
+    <a href="#" class="card-link">Créer</a>
+    <a href="#" class="card-link">Supprimer</a>
+  </div>
+</div>
+<!--
+~~~~~~~~~ | feuilles |~~~~~~~~~~
+-->
+<div class="card">
+  <div class="card-body  col-sm-12 col-md-5">
+    <h5 class="card-title">Feuilles</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Créer/Modifier une feuille d'emargement</h6>
+    <a href="#" class="card-link">Créer</a>
+    <a href="#" class="card-link">Modifier</a>
+  </div>
+</div>
 
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
-</head>
+<div class="card">
+  <div class="card-body  col-sm-12 col-md-5">
+    <h5 class="card-title">Feuilles</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Consulter</h6>
+    <a href="#" class="card-link">Voir feuille</a>
+  </div>
+</div>
 
-<body>
-  <span class="border border-primary"></span>
+<!--
+~~~~~~~~~ | Utilisateurs | ~~~~~~~~~
+-->
+<div class="card col-sm-12 col-md-6">
+  <div class="card-body">
+    <h5 class="card-title">Utilisateurs</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Créer/Modifier/Supprimer un utilisateur</h6>
+    <a href="#" class="card-link">Créer</a>
+    <a href="#" class="card-link">Modifier</a>
+    <a href="#" class="card-link">Supprimer</a>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
+  </div>
+</div>
+</div>
 
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-</body>
 
-</html>
+<?php
+include "template/footer.php";
+ ?>
