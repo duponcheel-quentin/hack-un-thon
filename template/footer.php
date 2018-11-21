e9715e2a7ee9b00b51ca9d446b9f44d8e7be9c7c
Vincent <vincentrichardeau@laposte.net>
2018-11-21 09:27:46 +0100

    first commit

--- template/footer.php
+++ template/footer.php
@@ -1,25 +1,7 @@
-<footer class="container">
-  <img src="https://www.adep-roubaix.fr/img/simplon.png" alt="simplon">
-  <img src="https://www.adep-roubaix.fr/img/logo_esaat.png" alt="Esaat">
-  <img src="https://www.adep-roubaix.fr/img/arofesep.png" alt="Arofesep">
-  <img src="https://www.adep-roubaix.fr/img/ecole-numerique.png" alt="Ecole numérique">
-  <img src="https://www.adep-roubaix.fr/img/mel.png" alt="MEL">
-  <img src="https://www.adep-roubaix.fr/img/nord.png" alt="nord département">
-  <img src="https://www.adep-roubaix.fr/img/region.png" alt="région">
-  <img src="https://www.adep-roubaix.fr/img/prefet.jpg" alt="prefet">
-  <img src="https://www.adep-roubaix.fr/img/cget.png" alt="cget">
-  <img src="https://www.adep-roubaix.fr/img/europe.jpg" alt="union européene">
-  <img src="https://www.adep-roubaix.fr/img/fse.jpeg" alt="slogan europe">
-  <img src="https://www.adep-roubaix.fr/img/roubaix.png" alt="ville de Roubaix">
-  <p><i class="fa fa-copyright" aria-hidden="true"> copyright 2018</i> - <a href="mentions légales">mentions légales</a></p>
-
-
-
-
-
-
-
-
+<footer>
+  <div class="copyright">
+    <p><i class="fa fa-copyright" aria-hidden="true"> copyright 2018</i> - <a href="mentions légales">mentions légales</a></p>
+  </div>
 </footer>
 <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
