f705d13ac8dadf95f34183f4f29a5bd2c04336a1
Vincent <vincentrichardeau@laposte.net>
2018-11-21 14:46:54 +0100

    add php

--- model/db.php
+++ model/db.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-   $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'phpmyadmin1', 'Azerty59');
+   $bdd = new PDO('mysql:host=localhost;dbname=Emargements;charset=utf8', 'phpmyadmin1', 'Azerty59');
 }
 catch (Exception $e)
 {
