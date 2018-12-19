1c88734cb7138f23fdf122c1038335ef743b9091
Votre Nom <Vous@exemple.com>
2018-12-19 11:28:03 +0100

    index.php modifided

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require "config/global.php";
 require "model/db.php";
 require "model/categoriesManagement.php";
-require "model/sheetManagement.php";
 require "model/usersManagement.php";
+require "model/sheetManagement.php";
 require "service/formChecker.php";
 require "service/router.php";
 require "service/sessionManager.php";
